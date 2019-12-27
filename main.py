b5808e3455dc16db723f54d4af63d166a930ad17
Amin <yazdi.mohammad99@gmail.com>
2019-12-27 18:40:20 +0330

    Report function.Read file.Before main Implementation

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 # OS Project - Memory Management
+#OS_CA_2_96243075
+import sys
+
+
+def report():
+    print("------------------------------------------")
+    print("Page Fault rate: {0.2f}".format((pf_count/count)))
+    print("TLB Hit rate: {0.2f}".format((tlb_count/count)))
+
+
+def convert_logical_to_physical(address):
+    pass
+
+
+''' Read file given from Argv
+print(sys.argv)
+file = open(sys.argv[1], "rt")
+'''
+try:
+    file = open("addresses.txt", "rt")
+except FileNotFoundError:
+    print("File Not Found in current directory")
+
+
+count = 0
+tlb_count = 0
+pf_count = 0
+for line in file:
+    count += 1
+    print(convert_logical_to_physical(line))
+report()
